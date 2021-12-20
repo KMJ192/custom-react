d7fd1601491cb071bbd7ceb1822aa4c43f5ead20
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-12-20 23:48:51 +0900

    Update diffingAlgorithm.ts

--- react/diffingAlgorithm/diffingAlgorithm.ts
+++ react/diffingAlgorithm/diffingAlgorithm.ts
@@ -5,8 +5,6 @@ function heuristicsAlgorithm(
   nextDom: customElement[],
 ) {
   if (prevDom.length === 0 || nextDom.length === 0) return;
-  console.log('prev', prevDom);
-  console.log('next', nextDom);
   if (prevDom.length === nextDom.length) {
     sameNodeCnt(prevDom, nextDom);
   } else {
@@ -14,7 +12,22 @@ function heuristicsAlgorithm(
   }
 }
 
-function sameNodeCnt(prevDom: customElement[], nextDom: customElement[]) {}
+function sameNodeCnt(prevDom: customElement[], nextDom: customElement[]) {
+  const len = prevDom.length;
+  for (let i = 0; i < len; i++) {
+    const {
+      tagName: prevTagName,
+      value: prevValue,
+      event: prevEvent,
+      props: prevProps,
+    } = prevDom[i];
+    const { tagName: nextTagName } = nextDom[i];
+    // if (JSON.stringify(prevDom[i]) !== JSON.stringify(nextDom[i])) {
+    //   console.log('update');
+    //   nextDom[i].dirty = true;
+    // }
+  }
+}
 
 function diffNodeCnt(prevDom: customElement[], nextDom: customElement[]) {}
 
